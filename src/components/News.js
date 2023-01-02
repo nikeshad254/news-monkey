0946914f1af0e93f4bceaf354a92fc5830bd2571
Nikesh Adhikari <110375186+ultimatum254@users.noreply.github.com>
2023-01-02 20:49:50 +0545

    top headlines skeleton made in newsitem and news

--- src/components/News.js
+++ src/components/News.js
@@ -4,9 +4,23 @@ import NewsItem from './NewsItem'
 export class News extends Component {
   render() {
     return (
-      <div>
+      <div className='container my-3'>
+        <h2>NewsMonkey - Top Headlines</h2>
         this is news components
-        <NewsItem/>
+        <div className="row">
+             <div className="col-md-4">
+                <NewsItem title="mytitle" description="my desc"/>
+             </div>
+
+             <div className="col-md-4">
+                <NewsItem title="mytitle" description="my desc"/>
+             </div>
+
+             <div className="col-md-4">
+                <NewsItem title="mytitle" description="my desc"/>
+             </div>
+             
+        </div>
       </div>
     )
   }
